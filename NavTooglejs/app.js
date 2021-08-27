5d33c68708c7e489f0d950e65eddbb33cee7eb07
xavier290 <xa25alex@gmail.com>
2021-08-26 21:07:08 -0600

    toggle effect added

--- NavTooglejs/app.js
+++ NavTooglejs/app.js
@@ -1,13 +1,16 @@
 const menuBtn = document.querySelector(".menu-btn");
+const MenuActive = document.querySelector(".navbar");
 
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
     menuBtn.classList.add("open");
+    MenuActive.classList.add("active");
     menuOpen = true;
   } else {
     menuBtn.classList.remove("open");
+    MenuActive.classList.remove("active");
     menuOpen = false;
   }
 });
