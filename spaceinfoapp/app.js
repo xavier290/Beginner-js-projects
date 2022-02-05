67585c63b59c8998ae962ed23211d22ce887b162
xavier290 <xa25alex@gmail.com>
2022-02-05 14:43:39 -0600

    refactoring js code

--- spaceinfoapp/app.js
+++ spaceinfoapp/app.js
@@ -25,16 +25,11 @@ function fecthingData() {
 }
 
 function displayingData(data) {
-    //console.log(data)
     const iframe = document.querySelector(".iframe-container")
     const copyright = document.querySelector(".copyright")
 
-    if(data.media_type == "image") {
-        iframe.innerHTML = `<img src=${data.url}>`
-    } else {
-        iframe.innerHTML = `<iframe src=${data.url} frameborder=0 allowfullscreen>`
-    }
-
+    iframe.innerHTML = data.media_type == "image" ? `<img src=${data.url}>` : `<iframe src=${data.url} frameborder=0 allowfullscreen>`
+    
     title.innerHTML = data.title 
     details.innerHTML = `<div>${data.explanation}</div>`
     copyright.textContent = "@COPYRIGHT: " + data.copyright
